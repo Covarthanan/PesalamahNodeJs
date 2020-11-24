91b3774313bbe48ab7b664820a5111afc908d02d
Covarthanan <covart1196@gmail.com>
2020-11-24 09:00:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
     console.log(`Server running at port : ${port}/`);
